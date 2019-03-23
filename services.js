cd1f008059dff76e7babb3eebe3d6fb8f95d78dc
Beto Farías <beto@2gom.com.mx>
2019-03-22 23:43:58 -0600

    1er servicio

--- services.js
+++ services.js
@@ -1,4 +1,6 @@
 
+//Import de la libreria de HTTPS
+const https = require('https');
 
 /**
  * Servicios para recuperar datos
@@ -6,9 +8,37 @@
  */
 //================== BTO ===============
 
+const URL = "https://hackaton.gruposicoss.com.mx/APIEnLinea/WSEnlinea.ashx?";
+
 /**
- *  
+ *  cuantas inasistencias han tenido los empleados en la ultima quincena
  */
+
+ function getInasistenciasForm(){
+    const QS = "accion=NLIncapacidades&r_FechaInicio=01/01/2016-31/03/2016";
+    
+    requestUrl(URL + QS);
+ }
 
 
  //================== IVAN ===============
+
+
+ //============ UTILIDADES ==============
+
+
+ function requestUrl(url){
+    https.get(url, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+
+    }).on('error', (e) => {
+        console.error(e);
+    });
+ }
+ 
+ getInasistenciasForm();
